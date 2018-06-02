ac1051d7d00011f1f028fac3bf2636adffbdf6a2
Iurii Bliuchak <zero.ukr@gmail.com>
2018-06-02 21:46:09 +0000

    Add rest of necessary endpoints with dummy data

--- cmd/options/main.go
+++ cmd/options/main.go
@@ -16,14 +16,17 @@ var (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", AppBanner).Methods("GET")
+	r.HandleFunc("/", AppBannerHandler).Methods("GET")
+	r.HandleFunc("/search/{id:[0-9]+}", GetSearchHandler).Methods("GET")
+	r.HandleFunc("/search", SetSearchHandler).Methods("POST")
+	r.HandleFunc("/search/{id:[0-9]+}", DeleteSearchHandler).Methods("DELETE")
 
 	fmt.Printf("Start %s on port %s", appName, port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 
-// AppBanner displays application basic information like name, version etc.
-func AppBanner(w http.ResponseWriter, r *http.Request) {
+// AppBannerHandler displays application basic information like name, version etc.
+func AppBannerHandler(w http.ResponseWriter, _ *http.Request) {
 	respRaw := make(map[string]string)
 	respRaw["app"] = "options"
 	resp, _ := json.Marshal(respRaw)
@@ -31,4 +34,40 @@ func AppBanner(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(resp)
+}
+
+// GetSearchHandler returns saved search request by ID
+func GetSearchHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	// stub response
+	// will be replaced by proper structure "pretty soon" (c)
+	respRaw := make(map[string]string)
+	respRaw = map[string]string{
+		"id":             vars["id"],
+		"reality_type":   "appartment",
+		"operation_type": "rent",
+		"country":        "cz",
+		"region":         "central",
+		"district":       "prague 1",
+		"area":           "32-45m^2",
+		"price":          "0-10000CZK",
+	}
+	resp, _ := json.Marshal(respRaw)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+// SetSearchHandler create/update search request
+func SetSearchHandler(w http.ResponseWriter, r *http.Request) {
+	// search request has been created/updated
+	w.WriteHeader(http.StatusCreated)
+}
+
+// DeleteSearchHandler delete search request by ID
+func DeleteSearchHandler(w http.ResponseWriter, r *http.Request) {
+	// search reqeust has been successfully deleted
+	w.WriteHeader(http.StatusNoContent)
 }
